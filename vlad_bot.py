4718b76c13511ffaf3d64ac10b893973d2999606
brunex92 <brunex92@gmail.com>
2017-10-13 14:04:49 -0300

    resolving identation errors

--- vlad_bot.py
+++ vlad_bot.py
@@ -35,9 +35,9 @@ def handle(msg):
 
     response = ["Risos", "Hausha7hausua7e","Calma","Nossa","Você tá bem, cara?", "Uia", "Te fode carlos", ".", "Egua" , "Nao ma", "Bacana", "Essa fera", "Que loucura", "O loco bichow", "Tenso", "Diz", "Rs", "E foi indo e foi indo... E tamo aqui agora", "Meu nome é Vladimir Lima tenho 24 anos, faço cerveja, ando de bicicleta e só", "Complicado esse humor", "boa", "Esse tempo todo?", "Vai fumar maconha ma\nQue tu fica de boas\nMenos exaltado"]
     responseAsk = ["Não sei, foda-se", "Que pergunta bosta, em? Vai se fuder", "Não, te fode", "Provavelmente nao em...", "Olha, eu acho que sim em...", "Dificil isso...", "Olha, talvez", "Sim! AEAEAEAEA!"]
-    
+
     if command == '/help' or command == '/help@vlademeeer_bot':
-    	bot.sendMessage(chat_id, "Vlad Bot v3.1\n\nBicho, a minha cabeça é difícil de entender. Da pra explicar não, foi mal. MAAAS, tem uns comandos bacanas aí oh...\n\n/vlad - faço um comentário extremamente enriquecedor para a conversa\n/askvlad - respostas honestas para qualquer pergunta\n/mito - minhas fotos sensuais que levam até homens a loucura\n/calma - CALMA SENHORA\n/diz - minha voz inconfundível pra vc se deliciar\n\n\nDeveloped by: Yuri Reis / Bruno Monteiro (Só fez um IF.. HEAUDHSADSDSDCVVFLFLFFL te amo cara)")
+        bot.sendMessage(chat_id, "Vlad Bot v3.1\n\nBicho, a minha cabeça é difícil de entender. Da pra explicar não, foi mal. MAAAS, tem uns comandos bacanas aí oh...\n\n/vlad - faço um comentário extremamente enriquecedor para a conversa\n/askvlad - respostas honestas para qualquer pergunta\n/mito - minhas fotos sensuais que levam até homens a loucura\n/calma - CALMA SENHORA\n/diz - minha voz inconfundível pra vc se deliciar\n\n\nDeveloped by: Yuri Reis / Bruno Monteiro (Só fez um IF.. HEAUDHSADSDSDCVVFLFLFFL te amo cara)")
     if command == '/calma' or command == '/calma@vlademeeer_bot':
         print ('Sending calma ...')
         f = open('/var/lib/openshift/573f29582d52714be30000dd/app-root/repo/calma.jpg', 'rb') 
@@ -48,12 +48,12 @@ def handle(msg):
         while op in filaVlad:
             op = random.randint(0,len(response))
         filaVlad.append(op)
-        
+
         print ('Sending msg ...')
-    	bot.sendMessage(chat_id, response[op])
-	if command == '/mito' or command == '/mito@vlademeeer_bot':
+        bot.sendMessage(chat_id, response[op])
+    if command == '/mito' or command == '/mito@vlademeeer_bot':
         img = random.randint(0,len(pics));
-        
+
         while img in filaMito:
             img = random.randint(0,len(pics));
         filaMito.append(img)
@@ -98,4 +98,4 @@ print ('Listening ...')
 # Keep the program running.
 while 1:
     time.sleep(10)
-    #chupa minha rola yuri
+    #chupa minha rola yuri aeaeae
