e5506a9244a17301b575ff851e8d632a1b398ca3
brunex92 <brunex92@gmail.com>
2017-10-17 17:59:19 -0300

    markdown, novo calma, fotos e audios nao repetem

--- vlad_bot.py
+++ vlad_bot.py
@@ -8,14 +8,15 @@ import random
 import os
 from collections import deque
 
+filaMito = deque(maxlen=20)
+filaDiz = deque(maxlen=5)
+filaVlad = deque(maxlen=5)
+
 TOKEN = os.environ['TELEGRAM_TOKEN']
 
 def handle(msg):
     chat_id = msg['chat']['id']
     command = msg['text']
-    filaMito = deque(maxlen=10)
-    filaDiz = deque(maxlen=5)
-    filaVlad = deque(maxlen=5)
 
     print ('Command ' + command.encode('utf-8') + ' received ...')
 
@@ -23,16 +24,22 @@ def handle(msg):
     pics = [f for f in os.listdir('/app/images/')]
     audios = [f for f in os.listdir('/app/sound/')]
 
-    response = ["Beisso aí","Risos", "Hausha7hausua7e","Calma","Nossa","Você tá bem, cara?", "Uia", "Te fode carlos", ".", "Egua" , "Nao ma", "Bacana", "Essa fera", "Que loucura", "O loco bichow", "Tenso", "Diz", "Rs", "E foi indo e foi indo... E tamo aqui agora", "Meu nome é Vladimir Lima tenho 24 anos, faço cerveja, ando de bicicleta e só", "Complicado esse humor", "boa", "Esse tempo todo?", "Vai fumar maconha ma\nQue tu fica de boas\nMenos exaltado"]
-    responseAsk = ["Não sei, foda-se", "Que pergunta bosta, em? Vai se fuder", "Não, te fode", "Provavelmente nao em...", "Olha, eu acho que sim em...", "Dificil isso...", "Olha, talvez", "Sim! AEAEAEAEA!"]
+    response = ["Gua", "Beisso aí","Risos", "Hausha7hausua7e", "Calma", "Nossa", "Você tá bem, cara?", "Uia", "Te fode carlos", ".", "Egua" , "Nao ma", "Bacana", "Essa fera", "Que loucura", "O loco bichow", "Tenso", "Diz", "Rs", "E foi indo e foi indo... E tamo aqui agora", "Meu nome é Vladimir Lima tenho 24 anos, faço cerveja, ando de bicicleta e só", "Complicado esse humor", "boa", "Esse tempo todo?", "Vai fumar maconha ma\nQue tu fica de boas\nMenos exaltado"]
+    responseAsk = ["Calma, explica aí q eu não entendi", "Q?", "Depende", "Não sei, foda-se", "Que pergunta bosta, em? Vai se fuder", "Não, te fode", "Provavelmente nao em...", "Olha, eu acho que sim em...", "Dificil isso...", "Olha, talvez", "Sim! AEAEAEAEA!", "Peraí que eu to comendo"]
 
     if command == '/help' or command == '/help@vlademeeer_bot':
-        bot.sendMessage(chat_id, "Vlad Bot v3.2\n\nBicho, a minha cabeça é difícil de entender. Da pra explicar não, foi mal. MAAAS, tem uns comandos bacanas aí oh...\n\n/vlad - faço um comentário extremamente enriquecedor para a conversa\n/askvlad - respostas honestas para qualquer pergunta\n/mito - minhas fotos sensuais que levam até homens a loucura\n/calma - CALMA SENHORA\n/diz - minha voz inconfundível pra vc se deliciar\n\n\nDeveloped by: Yuri Reis / Bruno Monteiro (Só fez um IF.. HEAUDHSADSDSDCVVFLFLFFL te amo cara)")
+        bot.sendMessage(chat_id, "*Vlad Bot v3.5*\n\nBicho, a minha cabeça é difícil de entender. Da pra explicar não, foi mal. MAAAS, tem uns comandos bacanas aí oh...\n\n*/vlad* - `faço um comentário extremamente enriquecedor para a conversa`\n*/askvlad* - `respostas honestas para qualquer pergunta`\n*/mito* - `minhas fotos sensuais que levam até homens a loucura`\n*/calma* - `CALMA SENHORA`\n*/diz* - `minha voz inconfundível pra vc se deliciar`\n\n\n*Developed by:* `Yuri Reis / Bruno Monteiro`", parse_mode='Markdown')
     
     if command == '/calma' or command == '/calma@vlademeeer_bot':
-        print ('Sending calma to chat: ' + repr(chat_id) + ' ...')
-        f = open('/app/calma.jpg', 'rb') 
-        bot.sendPhoto(chat_id, f)
+        calma = random.randint(0,1);
+        if calma == 0:
+            print ('Sending calma to chat: ' + repr(chat_id) + ' ...')
+            f = open('/app/calma.jpg', 'rb') 
+            bot.sendPhoto(chat_id, f)
+        elif calma == 1:
+            print ('Sending calma2 to chat: ' + repr(chat_id) + ' ...')
+            f = open('/app/calma2.jpg', 'rb') 
+            bot.sendPhoto(chat_id, f)
 
     if command == '/vlad' or command == '/vlad@vlademeeer_bot':
         op = random.randint(0,len(response)-1);
