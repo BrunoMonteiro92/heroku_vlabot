e1dc60fb2046b00311417ad65f53081657f645d5
BrunoMonteiro92 <bruno.mm1992@gmail.com>
2021-08-30 20:55:59 -0300

    Novo conteúdo / Testes pro usuário poder escolher Imagem/Som que são enviados

--- vlad_bot.py
+++ vlad_bot.py
@@ -1,8 +1,5 @@
 # coding=utf-8
-
-import sys
 import time
-import pprint
 import telepot
 import random
 import os
@@ -29,15 +26,25 @@ def handle(msg):
         pics = [f for f in os.listdir('/app/images/')]
         audios = [f for f in os.listdir('/app/sound/')]
 
-        response = ["Gua", "Beisso aí", "Risos", "Hausha7hausua7e", "Calma", "Te fode carlos", "Nossa", "Você tá bem, cara?", "Uia", "Te fode carlos", ".", "Egua", "Nao ma", "Bacana", "Essa fera", "Que loucura", "Te fode carlos", "O loco Bichow", "Tenso", "Diz",
-                    "Rs", "E foi indo e foi indo... E tamo aqui agora", "Meu nome é Vladimir Lima tenho 24 anos, faço cerveja, ando de bicicleta e só", "Complicado esse humor", "boa", "Esse tempo todo?", "Vai fumar maconha ma\nQue tu fica de boas\nMenos exaltado", "Bebam água"]
+        response = ["Gua", "Beisso aí", "Risos", "Hausha7hausua7e", "Calma", "Te fode carlos", "Nossa", "Você tá bem, cara?", "Uia", "Te fode carlos", ".", "Egua",
+                    "Nao ma", "Bacana", "Essa fera", "Que loucura", "Te fode carlos", "O loco Bichow", "Tenso", "Diz", "Rs", "E foi indo e foi indo... E tamo aqui agora",
+                    "Meu nome é Vladimir Lima tenho 24 anos, faço cerveja, ando de bicicleta e só", "Complicado esse humor", "boa", "Esse tempo todo?",
+                    "Vai fumar maconha ma\nQue tu fica de boas\nMenos exaltado", "Bebam água", "Opa", "Ma", "Huum", "Huuum", "Pera", "Tô com sono"]
         responseAsk = ["Calma, explica aí q eu não entendi", "Q?", "Depende", "Não sei, foda-se", "Que pergunta bosta, em? Vai se fuder", "Não, te fode",
-                       "Provavelmente nao em...", "Olha, eu acho que sim em...", "Dificil isso...", "Olha, talvez", "Sim! AEAEAEAEA!", "Peraí que eu to comendo"]
+                       "Provavelmente nao em...", "Olha, eu acho que sim em...", "Dificil isso...", "Olha, talvez", "Sim! AEAEAEAEA!", "Peraí que eu to comendo",
+                       "Café?", "Que o q ma", "Não sei ma", "Hein?", "É"]
 
         if command == '/help' or command == '/help@vlademeeer_bot':
             print('Command ' + command +
                   ' received from chat ' + repr(chat_id) + ' ...')
-            bot.sendMessage(chat_id, "*Vlad Bot v4.7*\n\nCara, a minha cabeça é difícil de entender. Da pra explicar não, foi mal. MAAAS, tem uns comandos bacanas aí oh...\n\n*/vlad* - faço um comentário extremamente enriquecedor para a conversa\n\n*/askvlad* - respostas honestas para qualquer pergunta\n\n*/fera* - minhas fotos sensuais que leva todo mundo a loucura\n\n*/calma* - CALMA SENHORA\n\n*/diz* - minha voz inconfundível pra vc se deliciar\n\n\n*Desenvolvido por:* Yuri Reis / Bruno Monteiro", parse_mode='Markdown')
+            bot.sendMessage(chat_id, "*Vlad Bot v5*\n\nCara, a minha cabeça é difícil de entender. Da pra explicar não, foi mal. MAAAS, tem uns comandos bacanas aí oh...\
+                            \n\n*/vlad* - faço um comentário extremamente enriquecedor para a conversa\
+                            \n\n*/askvlad* - respostas honestas para qualquer pergunta\
+                            \n\n*/fera* - minhas fotos sensuais que leva todo mundo a loucura\
+                            \n\n*/calma* - CALMA SENHORA\
+                            \n\n*/diz* - minha voz inconfundível pra vc se deliciar\
+                            \n\n\n*Desenvolvido por:* Yuri Reis / Bruno Monteiro",
+                            parse_mode='Markdown')
 
         if command == '/calma' or command == '/calma@vlademeeer_bot':
             print('Command ' + command +
@@ -100,25 +107,31 @@ def handle(msg):
                 with open('/app/images/%s' % img, 'rb') as f:
                     bot.sendPhoto(chat_id, f)
                 f.close()
-
-        '''elif '/fera' in command:
+        elif '/fera' in command:
             c, p = command.split(' ', 1)
             if not p.isdigit():
-                print (p + ' IS NOT A NUMBER !!!')
-                bot.sendMessage(chat_id, "Ma... Eu acho que *" + p.upper() + "* nao eh um numero " + emoji.emojize(":thinking_face:"), parse_mode='Markdown')
+                print(p + ' IS NOT A NUMBER !!!')
+                bot.sendMessage(chat_id, "Ma... Eu acho que *" +
+                                p.upper() + "* nao eh um numero", parse_mode='Markdown')
             else:
                 p = int(p)
                 if p < 0 or p > len(pics)-1:
-                    print (repr(p) + ' IS NOT IN RANGE !!!')
-                    bot.sendMessage(chat_id, "Ei... Éééééé... Tem que tá dentro do intervalo de *[0 até " + repr(len(pics)-1) + "]*", parse_mode='Markdown')
+                    print(repr(p) + ' IS NOT IN RANGE !!!')
+                    bot.sendMessage(chat_id, "Ei... Éééééé... Tem que tá dentro do intervalo de *[0 até " + repr(
+                        len(pics)-1) + "]*", parse_mode='Markdown')
                 else:
-                    with open('/app/images/%s' % pics[p], 'rb')
-                    print ('Sending pic ' + pics[p] + ' to chat: ' + repr(chat_id) + ' ...')
-
-                    bot.sendPhoto(chat_id, f)
-                    f.close()
-        '''
-
+                    if (pics[p].endswith('.gif')):
+                        print('Sending gif ***' + pics[p] +
+                              '*** to chat: ' + repr(chat_id) + ' ...')
+                        with open('/app/images/%s' % pics[p], 'rb') as f:
+                            bot.sendDocument(chat_id, f)
+                        f.close()
+                    else:
+                        print('Sending pic ***' + pics[p] +
+                              '*** to chat: ' + repr(chat_id) + ' ...')
+                        with open('/app/images/%s' % pics[p], 'rb') as f:
+                            bot.sendPhoto(chat_id, f)
+                        f.close()
         if command == '/mito' or command == '/mito@vlademeeer_bot':
             bot.sendMessage(
                 chat_id, 'Opa querido(a), esse comando foi substituído pelo /fera, por motivos de **FASCISMO**, perdão pelo incoveniente', parse_mode='Markdown')
@@ -150,23 +163,24 @@ def handle(msg):
             with open('/app/sound/%s' % aud, 'rb') as f:
                 bot.sendVoice(chat_id, f)
             f.close()
-            '''elif '/diz' in command:
-                c, a = command.split(' ', 1)
-                if not a.isdigit():
-                    print (a + ' IS NOT A NUMBER !!!')
-                    bot.sendMessage(chat_id, "Ma... Eu acho que *" + a.upper() + "* nao eh um numero ", parse_mode='Markdown')
+        elif '/diz' in command:
+            c, a = command.split(' ', 1)
+            if not a.isdigit():
+                print(a + ' IS NOT A NUMBER !!!')
+                bot.sendMessage(chat_id, "Ma... Eu acho que *" + a.upper() +
+                                "* nao eh um numero ", parse_mode='Markdown')
+            else:
+                a = int(a)
+                if a < 0 or a > len(audios)-1:
+                    print(repr(a) + ' IS NOT IN RANGE !!!')
+                    bot.sendMessage(chat_id, "Ei... Éééééé... Tem que tá dentro do intervalo de *[0 até " + repr(
+                        len(audios)-1) + "]*", parse_mode='Markdown')
                 else:
-                    a = int(a)
-                    if a < 0 or a > len(audios)-1:
-                        print (repr(a) + ' IS NOT IN RANGE !!!')
-                        bot.sendMessage(chat_id, "Ei... Éééééé... Tem que tá dentro do intervalo de *[0 até " + repr(len(audios)-1) + "]*", parse_mode='Markdown')
-                    else:
-                        with open('/app/sound/%s' % audios[a], 'rb')
-                        print ('Sending audio ' + audios[a] + ' to chat: ' + repr(chat_id) + ' ...')
-
-                        bot.sendVoice(chat_id, f)
-                        f.close()
-            '''
+                    with open('/app/sound/%s' % audios[a], 'rb') as f:
+                        print('Sending audio ' +
+                              audios[a] + ' to chat: ' + repr(chat_id) + ' ...')
+                    bot.sendVoice(chat_id, f)
+                    f.close()
     elif re.search(r'\btop[^ao]*\b', command, flags=re.IGNORECASE):
         print('Command TOP received from chat ' + repr(chat_id) + ' ...')
         print('Sending TOPPER to chat: ' + repr(chat_id) + ' ...')
